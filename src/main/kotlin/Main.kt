abcbf7fc2933b59062bb2ea0bc22ac52cc2a1209
Prabesh145 <niroulaprabesh414@gmail.com>
2022-05-15 12:15:59 +0545

    Printing in descending order

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,9 @@
 import java.awt.desktop.PrintFilesEvent
 
-//--------------Kotlin program to print from 1 to 50 using for loop--------------------------------
+//--------------Kotlin program to print from 50 to 1 using for loop--------------------------------
 
 fun main(){
-    for(i in 1..50){
+    for(i in 50 downTo 1){
         println(i)
     }
 }
-
-
-/*
-fun main(){
-    for (i in 1 until 51)
-        println(i)
-}
-*/
