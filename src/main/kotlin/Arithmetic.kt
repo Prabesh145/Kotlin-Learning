13c1dbd77311daa4c8a016e1cce77633bc77472a
Prabesh145 <niroulaprabesh414@gmail.com>
2022-05-22 17:31:16 +0545

    Class and object

---
+++ src/main/kotlin/Arithmetic.kt
@@ -0,0 +1,13 @@
+class Arithmetic {
+
+    var first:Int=0;
+    var second:Int=0;
+
+
+    fun math():Int{
+        return (first+second);
+    }
+
+
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,19 +3,11 @@ import java.awt.desktop.PrintFilesEvent
 //--------------Maps Introduction--------------------------------
 fun main(){
 
-    var name = arrayOf("Ram", "Shyam", "Hari")
-    var marks = arrayOf(1,3,4)
+  val arithmetic = Arithmetic()
+  arithmetic.first = 5
+  arithmetic.second = 7
 
-    var student = mapOf<String,Int>(
-        "Ram" to 1, "Shyam" to 3, "Hari" to 4
-
-    )
-
-    print("Enter the name of the student to find the marks")
-    var boy = readLine()!!
-
-    //var result = student[boy]
-    print("The result of the student $boy is: ${student[boy]} ")
+  print("The sum is: ${arithmetic.math()}")
 
 
 }
